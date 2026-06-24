f392065a2ef2b679c43158c130c398164f87e9e5
Waylan <waylan49@gmail.com>
2026-06-24 11:55:52 -0500

    Add soruce file for stripe.payment and freshness check rule

--- models/staging/stripe/stg_stripe_payments.sql
+++ models/staging/stripe/stg_stripe_payments.sql
@@ -1,6 +1,8 @@
 select 
     id as payment_id,
     orderid as order_id,
+    paymentmethod as payment_method,
     status,
-    AMOUNT/100 as amount
-from raw.stripe.payment
+    amount/100 as amount,
+    created as created_at
+from {{ source('stripe', 'payment') }}
